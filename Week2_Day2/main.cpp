0379bec848d4a217461dcfc9ff94f9e9c6205538
Michelle  Mellenthin <111709141+DrMellenthin@users.noreply.github.com>
2022-08-30 15:29:51 +0000

    prompt for input and read in vals

--- Week2_Day2/main.cpp
+++ Week2_Day2/main.cpp
@@ -2,4 +2,16 @@
 
 using namespace std;
 
-int main() { cout << "Hello World!\n"; }
+int main() {
+  // initialize variables
+  char ch1, ch2;
+  int val;
+
+  // prompt our user to type in "A 51"
+  cout << "Type the following \"A 51\":";
+
+  // read the values
+  cin >> ch1 >> ch2 >> val;
+
+  //
+}
